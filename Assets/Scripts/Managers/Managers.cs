3686d8fb6ee6b921fc7247961df3ff9ee7498d85
HyunWoo Kang <kuhn0305@gmail.com>
2022-10-21 23:52:53 +0900

    오류 수정
    싱글톤 패턴에서 최초 Init() 빠짐

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -6,7 +6,7 @@ public class Managers : MonoBehaviour
 {
     static Managers s_instance; // 유일성을 보장함
     // 유일한 매니저를 가지고 온다.
-    public static Managers Instance { get { return s_instance; } }
+    public static Managers Instance { get { Init(); return s_instance; } }
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,7 @@ public class Managers : MonoBehaviour
 
     static void Init()
     {
-        if(s_instance == null)
+        if (s_instance == null)
         {
             GameObject obect = GameObject.Find("@Managers");
 
