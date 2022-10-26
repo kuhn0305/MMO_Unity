d99d4cdc3e936845fb2ff2508894d9249f154a73
HyunWoo Kang <kuhn0305@gmail.com>
2022-10-26 20:14:43 +0900

    Vector 학습

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+
+// 1. 위치 벡터
+// 2. 방향 벡터
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+
+    //                 +
+    //        +       +
+    //  +-----------+
+
+    // 방향의 전체 크기
+    public float Magnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
+    // 크기가 1 짜리인 '단위 벡터'
+    public MyVector Normalized { get { return new MyVector(x / Magnitude, y / Magnitude, z / Magnitude); } }
+
+    public MyVector(float x, float y, float z)
+    { this.x = x; this.y = y; this.z = z; }
+
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static MyVector operator *(MyVector a, float b)
+    {
+        return new MyVector(a.x * b, a.y * b, a.z * b);
+    }
+}
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,7 +47,17 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
+        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
+        MyVector dir = to - from; // (5.0f, 0.0f, 0.0f) 방향벡터
 
+        dir = dir.Normalized; // (1.0f, 0.0f, 0.0f);
+
+        MyVector newPos = from + dir * _speed;
+
+        // 방향 벡터
+        // 1. 거리 (크기) 5
+        // 2. 실제 방향 ->
     }
 
     // Update is called once per frame
