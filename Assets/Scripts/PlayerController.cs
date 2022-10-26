7a462ce7e9e436f76bd99e6ef437712da9371e74
HyunWoo Kang <kuhn0305@gmail.com>
2022-10-26 19:15:03 +0900

    Position 학습

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    float _speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Local -> World :: TransformDirection
+        // World -> Local :: InverseTransformDirection
+
         if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+            transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.S))
-            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+            transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.A))
-            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
+
+        /*
+         * 
+         * Translate는 local 좌표 기준으로 움직임을 적용한다.
+         * 
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        */
     }
 }
